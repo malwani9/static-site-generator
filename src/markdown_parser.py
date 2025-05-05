2974e431d7dbeb58d7777f9d71b9af9f387e25c7
malwani <98039304+malwani9@users.noreply.github.com>
2025-05-05 19:15:16 +0300

    Add split_nodes_delimiter function to convert markdown to inline TextNode

---
+++ src/markdown_parser.py
@@ -0,0 +1,26 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+   new_nodes = []
+   for node in old_nodes:
+      if node.text_type != TextType.TEXT:
+         new_nodes.append(node)
+         continue
+      
+      split_nodes = []
+      sections = node.text.split(delimiter)
+
+      if len(sections) < 3 or len(sections) % 2 == 0:
+         raise Exception(f"Invalid markdown syntax, missing closing {delimiter}")
+      
+      for i in range(len(sections)):
+         if sections[i] == "":
+            continue
+         if i % 2 == 0:
+            split_nodes.append(TextNode(sections[i], TextType.TEXT, None))
+         else:
+            split_nodes.append(TextNode(sections[i], text_type, None))
+
+      new_nodes.extend(split_nodes)
+   return new_nodes
+
