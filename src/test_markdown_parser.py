d2f52897ce0dd0e195d1d0d225c0ef0ba978d69f
malwani <98039304+malwani9@users.noreply.github.com>
2025-05-06 18:18:26 +0300

    test: add test cases for extract_markdown_images/links

--- src/test_markdown_parser.py
+++ src/test_markdown_parser.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType, text_node_to_html_node
-from markdown_parser import split_nodes_delimiter
+from markdown_parser import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestMardownParser(unittest.TestCase):
     def test_equal_bold(self):
@@ -66,6 +66,30 @@ class TestMardownParser(unittest.TestCase):
             new_nodes,
         )
 
+    
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images2(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    def test_extract_markdown_linkss(self):
+        matches = extract_markdown_links(
+            "This is text with an [link](https://google.com)"
+        )
+        self.assertListEqual([("link", "https://google.com")], matches)
+
+    def test_extract_markdown_linkss(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
 
 if __name__ == "__main__":
     unittest.main()
