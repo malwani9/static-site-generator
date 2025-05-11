9b8dd4d76dc6d3f29e684b2bace98a3ada1f0c45
malwani <98039304+malwani9@users.noreply.github.com>
2025-05-11 20:41:20 +0300

    test: add test cases for text_to_textnode function

--- src/test_markdown_parser.py
+++ src/test_markdown_parser.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType, text_node_to_html_node
-from markdown_parser import split_nodes_delimiter, split_nodes_image, split_nodes_link, extract_markdown_images, extract_markdown_links
+from markdown_parser import split_nodes_delimiter, split_nodes_image, split_nodes_link, extract_markdown_images, extract_markdown_links, text_to_textnodes
 
 class TestMardownParser(unittest.TestCase):
     def test_equal_bold(self):
@@ -248,6 +248,26 @@ class TestMardownParser(unittest.TestCase):
                 ),
             ],
             new_nodes,
+        )
+
+
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        final = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            final,
         )
 
 if __name__ == "__main__":
