550f8e6c13c853dc6cf64b6fa5f936d3c9ca6478
malwani <98039304+malwani9@users.noreply.github.com>
2025-05-17 23:34:42 +0300

    add markdown_to_html function with few test cases

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,6 @@
+from htmlnode import HTMlNode, ParentNode, LeafNode
+from textnode import TextNode, text_node_to_html_node, TextType
+from markdown_parser import text_to_textnodes
 from enum import Enum
 
 def markdown_to_blocks(markdown):
@@ -19,7 +22,7 @@ class BlockType(Enum):
     ORDERED_LIST = "ordered_list"
 
 def block_to_block_type(block):
-    if block[0].isdigit() and block.startswith("# ", 1):
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
         return BlockType.HEADING
     if block.startswith("```") and block.count("```") == 2:
         return BlockType.CODE
@@ -41,3 +44,84 @@ def block_to_block_type(block):
               return BlockType.ORDERED_LIST
     
     return BlockType.PARAGRAPH
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children_nodes = []
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children_nodes.append(html_node)
+    
+    return ParentNode("div", children_nodes)
+
+def block_to_html_node(block):
+
+    block_type = block_to_block_type(block)
+
+    if block_type == BlockType.HEADING:
+        return heading_to_html(block)
+    elif block_type == BlockType.CODE:
+        return code_to_html(block)
+    elif block_type == BlockType.ORDERED_LIST:
+        return ordered_list_to_html(block)
+    elif block_type == BlockType.UNORDERED_LIST:
+        return unordered_list_to_html(block)
+    elif block_type == BlockType.QUOTE:
+        return quote_to_html(block)
+    elif block_type == BlockType.PARAGRAPH:
+        return paragraph_to_html_node(block)
+
+
+def heading_to_html(block):
+    level = block.count("#")
+    text = block[level+1:]
+    children_nodes = text_to_children(text)
+    return ParentNode(f"h{level}", children_nodes)
+
+def quote_to_html(block):
+    lines = block.split("\n")
+    children_nodes = []
+    for line in lines:
+        text = line[2:]
+        line_children = text_to_children(text)
+        children_nodes.extend(line_children)
+    return ParentNode("blockquote", children_nodes)
+
+
+def code_to_html(block):
+    text = block.replace("```", "")
+    text_node = TextNode(text, TextType.CODE, None)
+    return text_node_to_html_node(text_node)
+
+
+def ordered_list_to_html(block):
+    lines = block.split("\n")
+    children_nodes = []
+    for line in lines:
+        text = line[2:]
+        line_children = text_to_children(text)
+        children_nodes.extend(line_children)
+    return line_children
+
+def unordered_list_to_html(block):
+    lines = block.split("\n")
+    children_nodes = []
+    for line in lines:
+        text = line[2:]
+        line_children = text_to_children(text)
+        children_nodes.extend(line_children)
+    return line_children
+
+
+
+def paragraph_to_html_node(block):
+    children_nodes  = text_to_children(block)
+    return ParentNode("p", children_nodes)
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children_nodes = []
+    for text_node in text_nodes:
+        child = text_node_to_html_node(text_node)
+        children_nodes.append(child)
+    return children_nodes
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType, markdown_to_html_node
 
 class TestMarkdownBlocks(unittest.TestCase):
       def test_markdown_to_blocks(self):
@@ -93,7 +93,7 @@ This is the same paragraph on a new line
       
 
       def test_block_to_block_type_heading(self):
-            block = """1# This is heading"""
+            block = """# This is heading"""
             block_type = block_to_block_type(block)
             self.assertEqual(block_type, BlockType.HEADING)
       
@@ -127,10 +127,41 @@ This is the same paragraph on a new line
             self.assertEqual(block_type, BlockType.ORDERED_LIST)
      
       def test_block_to_block_type_paragraph(self):
-            block = """# How strange and foolish is man.
+            block = """How strange and foolish is man.
 He loses his health in gaining wealth. Then to regain health he wastes his wealth. 
 He ruins his present while worrying about his future but weeps in the future by recalling his past. 
 He lives as though death shall never come to him but dies in a way as if he were never born."""
             block_type = block_to_block_type(block)
             self.assertEqual(block_type, BlockType.PARAGRAPH)
-     
+
+
+
+      def test_headingblock(self):
+          md = """## this is **bold** heading"""
+
+          node = markdown_to_html_node(md)
+          html = node.to_html()
+          self.assertEqual(
+                html,
+                "<div><h2>this is <b>bold</b> heading</h2></div>",
+            )
+          
+      def test_paragraphblock(self):
+          md = """This is another paragraph with _italic_ text and `code` here"""
+
+          node = markdown_to_html_node(md)
+          html = node.to_html()
+          self.assertEqual(
+                html,
+                "<div><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+            ) 
+          
+      def test_quoteblock(self):
+          md = """> quote"""
+
+          node = markdown_to_html_node(md)
+          html = node.to_html()
+          self.assertEqual(
+                html,
+                "<div><blockquote>quote</blockquote></div>",
+            ) 
