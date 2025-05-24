961c41a9267f94731cdb78eb2347352c217fd926
malwani <98039304+malwani9@users.noreply.github.com>
2025-05-24 13:58:45 +0300

    configure delpoyment setting to run on github server

--- src/content_generator.py
+++ src/content_generator.py
@@ -3,9 +3,9 @@ from htmlnode import HTMlNode
 import os
 from pathlib import Path
 
-def generate_pages_recursive(content_path, template_path, dest_path):
-    for item in os.listdir(content_path):
-        src_path = os.path.join(content_path, item)
+def generate_pages_recursive(base_path, template_path, dest_path):
+    for item in os.listdir(base_path):
+        src_path = os.path.join(base_path, item)
         dst_path = os.path.join(dest_path, item)
         if os.path.isfile(src_path):
             new_dest_path = Path(dst_path).with_suffix(".html")
@@ -13,9 +13,9 @@ def generate_pages_recursive(content_path, template_path, dest_path):
         else:
             generate_pages_recursive(src_path, template_path, dst_path)
 
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generate page from from {from_path} to {dest_path} using {template_path}")
-    with open(from_path, "r") as from_file:
+def generate_page(base_path, template_path, dest_path):
+    print(f"Generate page from from {base_path} to {dest_path} using {template_path}")
+    with open(base_path, "r") as from_file:
        from_content = from_file.read()
     with open(template_path, "r") as template_file:
        template_content = template_file.read()
@@ -26,6 +26,11 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(from_content)
 
     final_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_code)
+
+    href_rerplace = "href=\"/"
+    src_replace = "src=\"/"
+
+    final_content = final_content.replace(href_rerplace, f"href=\"{base_path}").replace(src_replace, f"src=\"{base_path}")
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
--- src/main.py
+++ src/main.py
@@ -3,12 +3,15 @@ import shutil
 from textnode import TextNode, TextType
 from copystatic import copy_files_recursive
 from content_generator import generate_page, generate_pages_recursive
+import sys
 
 src = "./static" 
-dst = "./public"
-dest_path = "./public" 
+dst = "./docs"
+dest_path = "./docs" 
 from_path = "./content"
 template_path = "./template.html"
+
+base_path = sys.argv[0] or "/"
 
 def main():
     print("Deleting public directory...")
