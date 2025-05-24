5c5ced698b835949e748e2fdd7774cf45861d218
malwani <98039304+malwani9@users.noreply.github.com>
2025-05-24 14:21:20 +0300

    fix basepath issue

--- src/content_generator.py
+++ src/content_generator.py
@@ -3,19 +3,19 @@ from htmlnode import HTMlNode
 import os
 from pathlib import Path
 
-def generate_pages_recursive(base_path, template_path, dest_path):
-    for item in os.listdir(base_path):
-        src_path = os.path.join(base_path, item)
+def generate_pages_recursive(content_path, template_path, dest_path, base_path):
+    for item in os.listdir(content_path):
+        src_path = os.path.join(content_path, item)
         dst_path = os.path.join(dest_path, item)
         if os.path.isfile(src_path):
             new_dest_path = Path(dst_path).with_suffix(".html")
-            generate_page(src_path, template_path, new_dest_path)
+            generate_page(src_path, template_path, new_dest_path, base_path)
         else:
-            generate_pages_recursive(src_path, template_path, dst_path)
+            generate_pages_recursive(src_path, template_path, dst_path, base_path)
 
-def generate_page(base_path, template_path, dest_path):
-    print(f"Generate page from from {base_path} to {dest_path} using {template_path}")
-    with open(base_path, "r") as from_file:
+def generate_page(from_path, template_path, dest_path, base_path):
+    print(f"Generate page from from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as from_file:
        from_content = from_file.read()
     with open(template_path, "r") as template_file:
        template_content = template_file.read()
--- src/main.py
+++ src/main.py
@@ -5,23 +5,26 @@ from copystatic import copy_files_recursive
 from content_generator import generate_page, generate_pages_recursive
 import sys
 
-src = "./static" 
-dst = "./docs"
+static_path = "./static"
 dest_path = "./docs" 
 from_path = "./content"
 template_path = "./template.html"
 
-base_path = sys.argv[0] or "/"
+default_path = "/"
 
 def main():
-    print("Deleting public directory...")
-    if os.path.exists(dst):
-        shutil.rmtree(dst)
+    base_path = default_path
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
 
-    print("Copying static files to public dir...")
-    copy_files_recursive(src, dst)
+    print("Deleting public directory...")
+    if os.path.exists(dest_path):
+        shutil.rmtree(dest_path)
+
+    print("Copying static files to docs dir...")
+    copy_files_recursive(static_path, dest_path)
     
     print("generating page...")
-    generate_pages_recursive(from_path, template_path, dest_path)
+    generate_pages_recursive(from_path, template_path, dest_path, base_path)
 
 main()
